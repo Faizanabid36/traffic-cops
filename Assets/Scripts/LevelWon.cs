39412edb70cbdb088da84439926109cfc6f95468
RiffatS <riffatsiddiqui36@gmail.com>
2022-02-18 11:03:04 +0500

    level won

--- Assets/Scripts/LevelWon.cs
+++ Assets/Scripts/LevelWon.cs
@@ -2,18 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine. SceneManagement;
 
 public class LevelWon : MonoBehaviour
 {
     public Text levelcompleted;
-    // Start is called before the first frame update
     
 
-      void OnTriggerEnter(Collider other) {
+    void OnTriggerEnter(Collider other) {
          if(other.gameObject.tag=="Player"){
-             Debug.Log("collided");
+           
              levelcompleted.gameObject.SetActive(true);
-            GameManager.levelWon=true;
+            GameManager.nextLevel=true;
+            // Debug.Log(("Scene Count: " + SceneManager.sceneCountInBuildSettings));
          }
          
      }
