df4a77243ba6b8810c544b56f3fc10400f639006
RiffatS <riffatsiddiqui36@gmail.com>
2022-02-20 20:58:58 +0500

    audio

---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Settings : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    // Start is called before the first frame update
+    public void Volume(float volume){
+        audioMixer.SetFloat("Volume",volume);
+    }
+}
