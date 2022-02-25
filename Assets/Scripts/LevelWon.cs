9b776029c26accc221c4c17e490a33d1ca022f81
RiffatS <riffatsiddiqui36@gmail.com>
2022-02-25 12:47:01 +0500

    level3 details

--- Assets/Scripts/LevelWon.cs
+++ Assets/Scripts/LevelWon.cs
@@ -14,6 +14,7 @@ public class LevelWon : MonoBehaviour
     {
         if (other.gameObject.tag == "Player")
         {
+            
             GameManager.levelCompleted = true;
             UIManager.Instance.LevelCompleted();
             StartCoroutine(StopForSecsCoroutine());
